59945d47db39774f0303e3c072f6f6de833dd58a
Yonas.B <143127905+YonasBetizazu@users.noreply.github.com>
2024-10-27 15:59:50 +0100

    Create task_6.sql

---
+++ task_6.sql
@@ -0,0 +1,18 @@
+USE alx_book_store;
+INSERT INTO customers (customer_id,customer_name,email,address ) 
+VALUES(customer_id = 1,
+customer_name = "Cole Baidoo",
+email = "cbaidoo@sandtech.com",
+address = "123 Happiness Ave."),
+  (customer_id = 2,
+     customer_name = "Blessing Malik",
+     email = "bmalik@sandtech.com",
+     address = "124 Happiness  Ave."),
+     (customer_id = 3,
+     customer_name = "Obed Ehoneah",
+     email = "eobed@sandtech.com",
+     address = "125 Happiness  Ave."),
+     (customer_id = "4",
+     customer_name = "Nehemial Kamolu",
+     email = "nkamolu@sandtech.com",
+     address = "126 Happiness  Ave.")
